2a1aadf654e0445bc6bf981a67c959f32043d40d
Ramesh Mudavath <ramesh.mudavath@enfec.com>
2025-04-22 15:14:04 +0530

    first commit for the first version

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__,static_folder='abs')
+
+@app.route('/')
+def home():
+    return send_from_directory(app.static_folder,'index.html')
+
+@app.route('/about.html')
+def about():
+    return send_from_directory(app.static_folder,'about.html')
+
+@app.route('/contact.html')
+def contact():
+    return send_from_directory(app.static_folder, 'contact.html')
+
+@app.route('/services.html')
+def services():
+    return send_from_directory(app.static_folder, 'services.html')
+
+@app.route('/blogs.html')
+def blogs():
+    return send_from_directory(app.static_folder, 'blogs.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
